985f113b0e5f6488d0062b780bab2f6adf1a31a8
Thomas Kraemer <thomas.kraemer@desy.de>
2007-01-16 15:59:33 +0000

    print additional information in debug mode.

--- src/ITreeFactoryROOT.cc
+++ src/ITreeFactoryROOT.cc
@@ -7,6 +7,13 @@ using namespace std;
 
 ITree * ITreeFactoryROOT::create()
 {
+#ifdef USE_RAIDA_DEBUG_VERBOSE
+  cout << "ITreeFactoryROOT::create():" << endl;
+  cout << "          " 
+       << "Creates an AIDA tree in memory" << endl;
+  cout << "          " 
+       << "will call: ITreeROOT::ITreeROOT()" << endl;
+#endif
   return new ITreeROOT;
 }
 
@@ -16,5 +23,25 @@ ITree * ITreeFactoryROOT::create(const std::string & storeName,
 				 bool createNew, 
 				 const std::string &options)
 {
+#ifdef USE_RAIDA_DEBUG_VERBOSE
+  cout << "ITreeFactoryROOT::create(const std::string & storeName,const std::string & storeType,bool readOnly,bool createNew,const std::string &options):" << endl;
+  cout << "          " 
+       << "Creates an AIDA tree connected to a ROOT file" << endl << endl;
+  cout << "          " 
+       << "parameter:" << endl;
+  cout << "          " 
+       << "---------" << endl;
+  cout << "          " 
+       << "storeName: " << storeName << endl;
+  cout << "          " 
+       << "storeType: " << storeType << endl;
+  cout << "          " 
+       << "readOnly: " << readOnly 
+       << " createNew: " << createNew << endl;
+  cout << "          " 
+       << "options: " << options << endl;
+  cout << "          " 
+       << "will call: ITreeROOT::ITreeROOT(const std::string & storeName,const std::string & storeType,bool readOnly,bool createNew,const std::string &options)" << endl;
+#endif
   return new ITreeROOT(storeName,storeType,readOnly,createNew,options);
 }
